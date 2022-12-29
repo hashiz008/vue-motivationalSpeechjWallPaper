0ce13d5010f6a175b2560176e232f2640ffa8460
橋本栄介 <hashiz008@hashimotoeisukenoMacBook-Pro.local>
2022-12-29 18:12:01 +0900

    Vue.js版 motivationalSpeechWallPaper完成

---
+++ src/components/wallPaper1.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const text = "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Sain";
+const imgs = "https://recursionist.io/img/different-job.png";
+</script>
+
+<template>
+    <div class="container">
+        <img :src="imgs" class="imgStyle">
+        <p class="message">{{ text }}</p>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display:flex;
+    text-align: center;
+    align-items: center;
+    flex-direction:column;
+}
+.imgStyle {
+    width:50rem;
+    height:40rem;
+}
+.message {
+    display:flex;
+    align-items: center;
+    position: absolute;
+    color:#2c3e50;
+    width:400px;
+    top:100px;
+}
+</style>
---
+++ src/components/wallPaper3.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const text = "Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman";
+const imgs = "https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg";
+</script>
+
+<template>
+    <div class="container">
+        <img :src="imgs" class="imgStyle">
+        <p class="message">{{ text }}</p>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display:flex;
+    text-align: center;
+    align-items: center;
+    flex-direction:column;
+}
+.imgStyle {
+    width:50rem;
+    height:40rem;
+}
+.message {
+    display:flex;
+    justify-content:flex-end;
+    position: absolute;
+    color:#2c3e50;
+    width:300px;
+}
+</style>
